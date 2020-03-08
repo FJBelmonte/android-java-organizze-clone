2491e04bb41eb8cc380674405a79125783947379
Fernando Belmonte <fernando.belmonte@live.com>
2020-03-07 21:48:04 -0300

    setup: home components

--- app/src/main/java/com/coffeit/organizze/activity/HomeActivity.java
+++ app/src/main/java/com/coffeit/organizze/activity/HomeActivity.java
@@ -10,13 +10,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.TextView;
 
 import com.coffeit.organizze.R;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private MaterialCalendarView calendarView;
+    private TextView textWelcome, textTotal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        calendarView.findViewById(R.id.calendarView);
+        textWelcome.findViewById(R.id.textWelcome);
+        textTotal.findViewById(R.id.textTotal);
+
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+
+            }
+        });
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
